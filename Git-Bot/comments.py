b8ade8c087c054020a1276430cf950cd0b6dbe80
Aditya-Ohri <ohri.aditya@yahoo.ca>
2019-09-14 10:42:37 -0400

    server work

--- Git-Bot/comments.py
+++ Git-Bot/comments.py
@@ -4,9 +4,10 @@ import json
 
 def create_comment(org_name, repo_name, issue_name, username, password, text):
     url = "https://api.github.com/repos/" + org_name + "/" + repo_name + "/issues/" + issue_name + "/comments"
+    dict_content = {"body": text}
     session = requests.session()
     session.auth = (username, password)
-    req = session.post(url, json.dumps(text))
+    req = session.post(url, json.dumps(dict_content))
     res = req.text
     res_json = req.json()
     return res
--- Git-Bot/server.py
+++ Git-Bot/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 USERNAME = "Algo-Bot"
 PASSWORD = "AlgoFund2019HTN"
 
-TEST_STRING = "/new_bounty"
+#TEST_STRING = "/new_bounty"
 
 
 @app.route('/new_bounty/<org_name>/<repo_name>/<issue_name>/', methods=['POST'])
@@ -23,6 +23,31 @@ def new_bounty(org_name, repo_name, issue_name):
         return req
 
 
+@app.route('/new_applicant/<org_name>/<repo_name>/<issue_name>', methods=['POST'])
+def new_applicant(org_name, repo_name, issue_name):
+    if request.method == 'POST':
+        name = request.args.get('name')
+        link = request.args.get('link')
+        desc = request.args.get('description')
+        content = "### " + name + ", " + link + " has applied for this bounty: " + desc + "."
+        req = create_comment(org_name, repo_name, issue_name, USERNAME, PASSWORD, content)
+        return req
+
+
+@app.route('/work_submitted/<org_name>/<repo_name>/<issue_name>', methods=['POST'])
+def work_submitted(org_name, repo_name, issue_name):
+    if request.method == 'POST':
+        name = request.args.get('name')
+        link = request.args.get('link')
+        val = request.args.get('val')
+        pr_num = request.args.get('pr_num')
+        pr_url = "https://github.com/" + org_name + "/" + repo_name + "/pulls/" + pr_num
+        content = "### " + name + ", " + link + " has submitted a bounty of value " + val + ". Please see his/her PR here: " + \
+                  pr_url + "."
+        req = create_comment(org_name, repo_name, issue_name, USERNAME, PASSWORD, content)
+        return req
+
+
 @app.route('/work_finished/<org_name>/<repo_name>/<issue_name>/', methods=['POST'])
 def work_finished(org_name, repo_name, issue_name):
     if request.method == 'POST':
@@ -31,8 +56,8 @@ def work_finished(org_name, repo_name, issue_name):
         name_2 = request.args.get('name_2')
         link_2 = request.args.get('link_2')
         value = request.args.get('value')
-        content = "### " + name_1 + ", " + link_1 + " has paid out " + name_2 + ", " + link_2 + " for this bounty valued" \
-                                                                                                "at " + value + "."
+        content = "### " + name_1 + ", " + link_1 + " has paid out " + name_2 + ", " + link_2 + " for this bounty valued" + \
+                  "at " + value + "."
         req = create_comment(org_name, repo_name, issue_name, USERNAME, PASSWORD, content)
         return req
 
